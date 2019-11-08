53c7c71cef611f39c4fdac3a5f25018a90e4810e
Anuj-Chaturvedi <57126878+Anuj-Chaturvedi@users.noreply.github.com>
2019-11-08 23:17:36 +0530

    Separating Backend and Servlet

--- UploadHandlerServlet.java
+++ UploadHandlerServlet.java
@@ -1,4 +1,4 @@
-package assignment;
+package Servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,99 +6,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.*;
-
 import java.io.*;
 import java.util.*;
 
-import jxl.*;
-import jxl.read.biff.BiffException;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.apache.commons.io.FilenameUtils;
+
+import Backend.UploadToDatastore;
 
 @SuppressWarnings("serial")
 @WebServlet(
-    name = "UploadHandlerServlet",
-    urlPatterns = {"/UploadHandlerServlet"}
+    	name = "UploadHandlerServlet",
+    	urlPatterns = {"/UploadHandlerServlet"}
 )
 
 public class UploadHandlerServlet extends HttpServlet {
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException, ServletException {
-	
-	  FileItemFactory factory = new DiskFileItemFactory();
-	  ServletFileUpload upload = new ServletFileUpload(factory);
-	  
-	  List<FileItem> items;
-	  boolean status = true;
-	  
-	  try {
-		    String saveFile=System.getProperty("java.io.tmpdir");
-		    items = upload.parseRequest(request);
-		    Iterator<FileItem> itr = items.iterator();
-		    FileItem item = (FileItem)itr.next();
-		    String itemname = item.getName();
-		    String filename = FilenameUtils.getName(itemname);
-		    File f = new File(saveFile+"/"+filename);
-		    item.write(f);
-		    status=readExcel(f);
-		    f.delete();
-	    } 
-	    catch (Exception e) {
-		    // TODO Auto-generated catch block
-		    e.printStackTrace();
-		    status = false;
-	    }
-        if(status)        	
-        	response.sendRedirect("/login.jsp");
-        else
-        	response.getWriter().print("Some Exception occurred...");
-    }
-
-    public boolean readExcel(File f) {
-
-    	try {
-    		
-    		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-			Workbook workBook = Workbook.getWorkbook(f);
-			String [] sheetNames = workBook.getSheetNames();
-			Sheet sheet=null;
-			
-			for (int sheetNumber =0; sheetNumber < sheetNames.length; sheetNumber++) {
-				
-				int key=1;
-				sheet=workBook.getSheet(sheetNames[sheetNumber]);
-				
-				for (int rows=1;rows < sheet.getRows();rows++) {
-					
-					if(!sheet.getCell(0, rows).getContents().equals("")) {
-					    Entity e = new Entity("User",key++);
-					    
-				        for (int columns=0;columns < sheet.getColumns();columns++)
-					        e.setProperty(sheet.getCell(columns, 0).getContents(),
-							            sheet.getCell(columns, rows).getContents());
-				        ds.put(e);
-				    }
-		        }
-			}
+    	@Override
+    	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+      		throws IOException, ServletException 
+	{
+    		boolean status = true;
+    		FileItemFactory factory = new DiskFileItemFactory();
+    		ServletFileUpload upload = new ServletFileUpload(factory);
+    		try {
+			List<FileItem> items = upload.parseRequest(request);
+			status = UploadToDatastore.start(items);
 		} 
-    	catch (BiffException | IOException e) {
+		catch (FileUploadException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return false;
+			status = false;
 		}
-    	return true;
-    }
-    
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-    	throws IOException {
-    	    response.sendRedirect("/index.html");
-    }
+        	if(status)        	
+        		response.sendRedirect("/login.jsp");
+        	else
+        		response.getWriter().print("Some Exception occurred...");
+    	}
+
+    	@Override
+    	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws IOException 
+	{
+    	    	response.sendRedirect("/index.html");
+    	}
 }
