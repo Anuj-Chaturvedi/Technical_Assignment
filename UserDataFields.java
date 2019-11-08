e7d092448b3d602be53d9fcd65d4bc459df05d90
Anuj-Chaturvedi <57126878+Anuj-Chaturvedi@users.noreply.github.com>
2019-11-08 23:12:02 +0530

    Data model for Fields that can be stored in DataStore and their types

---
+++ UserDataFields.java
@@ -0,0 +1,12 @@
+package DataModel;
+
+import java.util.Date;
+
+public class UserDataFields {
+	private String Name;
+	private String Password;
+	private String College;
+	private Date DOB;
+	private String Email;
+	private long Phone;
+}
